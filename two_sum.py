0e9c4aba874552abe80bd7208c0bb3371c06cc6a
Morayo <morayo.bamgbose@cirrusage.com>
2019-12-26 08:43:23 +0100

    fix issues

--- two_sum.py
+++ two_sum.py
@@ -1,13 +1,19 @@
 class Solution(object):
     def twoSum(self, nums, target):
         indices = []
-        for num in nums:
+        if(len(nums) == 2):
+            return [0,1]
+        
+        for i,num in enumerate(nums):
             match = target - num
             
             if(match in nums):
-                indices.append(nums.index(match))
-                indices.append(nums.index(num))
-                break
-                               
+                first_number_index = nums.index(match)
+                current_index = i
+                if(first_number_index != current_index):
+                    indices.append(first_number_index)
+                    indices.append(current_index)
+                    break
+                                 
         return indices               
         
